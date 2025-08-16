e0adb6d8777fdd2c93b5196ac155626d49efcc3f
Grzegorz Danowski <grzegorz.danowski@ewl.com.pl>
2025-08-16 15:13:32 +0200

    Add github actions

--- cmd/micam-watcher/main.go
+++ cmd/micam-watcher/main.go
@@ -2,6 +2,21 @@ package main
 
 import "github.com/GrzesiekGdn/micam-watcher/internal/platform"
 
+import (
+    "flag"
+    "fmt"
+)
+
+var version = "dev" // will be replaced by ldflags
+
 func main() {
+    showVersion := flag.Bool("version", false, "Print version and exit")
+    flag.Parse()
+
+    if *showVersion {
+        fmt.Println("micam-watcher version", version)
+        return
+    }
+	
 	platform.StartService()
 }
